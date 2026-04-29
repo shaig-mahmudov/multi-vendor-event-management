ce0befe8487b94bc55df024de22fc38f05dad1e6
IslamSamadov <islamsamedov52@gmail.com>
2026-04-29 14:11:36 -0600

    add role-based access control and helper methods for event organizers

--- event-management/src/main/java/org/ironhack/project/eventmanagement/service/organizer/impl/EventOrganizerServiceImpl.java
+++ event-management/src/main/java/org/ironhack/project/eventmanagement/service/organizer/impl/EventOrganizerServiceImpl.java
@@ -2,16 +2,17 @@ package org.ironhack.project.eventmanagement.service.organizer.impl;
 
 import org.ironhack.project.eventmanagement.dto.request.organizer.AddOrganizerRequest;
 import org.ironhack.project.eventmanagement.dto.response.OrganizerResponse;
-import org.ironhack.project.eventmanagement.entity.Event;
-import org.ironhack.project.eventmanagement.entity.EventOrganizer;
-import org.ironhack.project.eventmanagement.entity.Vendor;
+import org.ironhack.project.eventmanagement.entity.*;
 import org.ironhack.project.eventmanagement.exception.BadRequestException;
 import org.ironhack.project.eventmanagement.exception.ConflictException;
 import org.ironhack.project.eventmanagement.exception.NotFoundException;
+import org.ironhack.project.eventmanagement.exception.UnauthorizedException;
 import org.ironhack.project.eventmanagement.repository.EventOrganizerRepository;
 import org.ironhack.project.eventmanagement.repository.EventRepository;
+import org.ironhack.project.eventmanagement.repository.UserRepository;
 import org.ironhack.project.eventmanagement.repository.VendorRepository;
 import org.ironhack.project.eventmanagement.service.organizer.EventOrganizerService;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,12 +21,17 @@ import java.util.List;
 public class EventOrganizerServiceImpl implements EventOrganizerService {
     private final EventOrganizerRepository organizerRepository;
     private final EventRepository eventRepository;
-    private VendorRepository vendorRepository;
+    private final VendorRepository vendorRepository;
+    private final UserRepository userRepository;
 
-    public EventOrganizerServiceImpl(EventOrganizerRepository organizerRepository, EventRepository eventRepository, VendorRepository vendorRepository) {
+    public EventOrganizerServiceImpl(EventOrganizerRepository organizerRepository,
+                                     EventRepository eventRepository,
+                                     VendorRepository vendorRepository,
+                                     UserRepository userRepository) {
         this.organizerRepository = organizerRepository;
         this.eventRepository = eventRepository;
         this.vendorRepository = vendorRepository;
+        this.userRepository = userRepository;
     }
 
     // add organizer
@@ -34,11 +40,24 @@ public class EventOrganizerServiceImpl implements EventOrganizerService {
         Event event = eventRepository.findById(eventId)
                 .orElseThrow(() -> new NotFoundException("Event not found"));
 
+        User user = getCurrentUser();
+
+        if(!user.getRole().equals(Role.ADMIN)){
+            // getting current vendor
+            Vendor currentVendor = getCurrentVendor();
+            checkOwnership(eventId, currentVendor.getId());
+        }
+
+        // getting target vendor
         Vendor vendor = vendorRepository.findById(request.getVendorId())
                 .orElseThrow(() -> new NotFoundException("Vendor Not Found"));
 
         if(organizerRepository.existsByEventIdAndVendorId(eventId, vendor.getId())){
             throw new ConflictException("Organizer already exists for this event");
+        }
+
+        if(request.getRole() == OrganizerRole.MAIN){
+            throw new BadRequestException("Use transfer endpoint to assign MAIN role(current owner will lose access to event and become co-organizer)");
         }
 
         EventOrganizer organizer = new EventOrganizer();
@@ -59,6 +78,111 @@ public class EventOrganizerServiceImpl implements EventOrganizerService {
     // get organizers
     @Override
     public List<OrganizerResponse> getOrganizers(Long eventId) {
+        Event event = eventRepository.findById(eventId)
+                .orElseThrow(() -> new NotFoundException("Event not found"));
+
+        User user = getCurrentUser();
+
+        if(!user.getRole().equals(Role.ADMIN)){
+            return map(eventId);
+        }
+
+        if (event.getStatus() == EventStatus.PUBLISHED) {
+            return map(eventId);
+        }
+
+        Vendor vendor = getCurrentVendor();
+        checkOwnership(eventId, vendor.getId());
+
+        return map(eventId);
+    }
+
+    // remove organizer
+    @Override
+    public void removeOrganizer(Long organizerId) {
+        EventOrganizer organizer = organizerRepository.findById(organizerId)
+                        .orElseThrow(() -> new NotFoundException("Organizer not found"));
+
+        User user = getCurrentUser();
+
+        if(!user.getRole().equals(Role.ADMIN)){
+            Vendor currentVendor = getCurrentVendor();
+            checkOwnership(organizer.getEvent().getId(), currentVendor.getId());
+        }
+
+        if (organizer.getRole() ==  OrganizerRole.MAIN) {
+            throw new BadRequestException("Cannot remove MAIN organizer");
+        }
+
+        organizerRepository.delete(organizer);
+    }
+
+    @Override
+    public void transferOwnership(Long eventId, Long newVendorId) {
+        Event event = eventRepository.findById(eventId)
+                .orElseThrow(() -> new NotFoundException("Event not found"));
+
+        User user = getCurrentUser();
+
+        EventOrganizer currentMain;
+
+        if(user.getRole().equals(Role.ADMIN)){
+            currentMain = organizerRepository
+                    .findByEventIdAndRole(eventId, OrganizerRole.MAIN)
+                    .orElseThrow(() -> new NotFoundException("Main organizer not found"));
+        }else{
+            Vendor currentVendor = getCurrentVendor();
+            currentMain = organizerRepository
+                    .findByEventIdAndVendorId(eventId, currentVendor.getId())
+                    .orElseThrow(() -> new NotFoundException("Main organizer not found"));
+            if(currentMain.getRole() != OrganizerRole.MAIN){
+                throw new UnauthorizedException("You are not owner of this event");
+            }
+        }
+
+        EventOrganizer newOwner = organizerRepository
+                .findByEventIdAndVendorId(eventId, newVendorId)
+                .orElseThrow(() -> new BadRequestException("New owner must already be an organizer"));
+
+        if (newOwner.getRole() == OrganizerRole.MAIN) {
+            throw new ConflictException("Already MAIN organizer");
+        }
+
+        currentMain.setRole(OrganizerRole.CO_ORGANIZER);
+        newOwner.setRole(OrganizerRole.MAIN);
+
+        organizerRepository.saveAll(List.of(currentMain,newOwner));
+    }
+
+    // helper methods
+    private User getCurrentUser() {
+        var auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth == null || !auth.isAuthenticated() || auth.getName() == null) {
+            throw new UnauthorizedException("Not authenticated");
+        }
+
+        return userRepository.findByEmail(auth.getName())
+                .orElseThrow(() -> new UnauthorizedException("User not found"));
+    }
+
+    private Vendor getCurrentVendor() {
+        User user = getCurrentUser();
+
+        return vendorRepository.findByUserId(user.getId())
+                .orElseThrow(() -> new BadRequestException("User is not a vendor"));
+    }
+
+    private void checkOwnership(Long eventId, Long vendorId) {
+        boolean isOwner = organizerRepository
+                .existsByEventIdAndVendorIdAndRole(eventId, vendorId, OrganizerRole.MAIN);
+
+        if (!isOwner) {
+            throw new UnauthorizedException("You are not owner of this event");
+        }
+    }
+
+    private List<OrganizerResponse> map(Long eventId) {
         return organizerRepository.findByEventId(eventId)
                 .stream()
                 .map(o -> new OrganizerResponse(
@@ -68,14 +192,5 @@ public class EventOrganizerServiceImpl implements EventOrganizerService {
                         o.getRole()
                 ))
                 .toList();
-    }
-
-    // remove organizer
-    @Override
-    public void removeOrganizer(Long organizerId) {
-        EventOrganizer organizer = organizerRepository.findById(organizerId)
-                        .orElseThrow(() -> new NotFoundException("Organizer not found"));
-
-        organizerRepository.delete(organizer);
     }
 }
