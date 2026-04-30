20910279177af4174dcbff8cdde46430ba15f084
IslamSamadov <islamsamedov52@gmail.com>
2026-05-01 02:57:35 +0400

    implement ticket category RBAC

--- event-management/src/main/java/org/ironhack/project/eventmanagement/service/ticket/TicketCategoryService.java
+++ event-management/src/main/java/org/ironhack/project/eventmanagement/service/ticket/TicketCategoryService.java
@@ -1,19 +1,20 @@
 package org.ironhack.project.eventmanagement.service.ticket;
 
 import org.ironhack.project.eventmanagement.dto.request.ticket.CreateTicketCategoryRequest;
+import org.ironhack.project.eventmanagement.dto.response.TicketCategoryResponse;
 import org.ironhack.project.eventmanagement.entity.TicketCategory;
 
 import java.util.List;
 
 public interface TicketCategoryService {
 
-    TicketCategory create(CreateTicketCategoryRequest request);
+    TicketCategory create(Long eventId, CreateTicketCategoryRequest request);
 
-    TicketCategory getById(Long id);
+    List<TicketCategory> getByEventId(Long eventId);
 
     List<TicketCategory> getAll();
 
-    void delete(Long id);
+    void delete(Long eventId, Long id);
 
     void validateAvailability(Long id, int quantity);
 
